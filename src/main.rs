b012abdfc779da7e8e0900fe8f559945b68487c6
Lucas Pickering <lucas@lucaspickering.me>
2025-06-02 08:34:10 -0400

    Add tab layout

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,71 @@
-use iced::{
-    Alignment, Element, Length,
-    widget::{Button, Column, Row, Text, TextInput},
-};
+use std::fmt::Display;
+
+use iced::{Element, Length, widget::Column};
 use iced_aw::{TabBar, TabLabel, iced_fonts};
 
 fn main() -> iced::Result {
-    iced::application(
-        "Tab Bar example",
-        TabBarExample::update,
-        TabBarExample::view,
-    )
-    .font(iced_fonts::REQUIRED_FONT_BYTES)
-    .run()
+    iced::application("Gruber", State::update, State::view)
+        .font(iced_fonts::REQUIRED_FONT_BYTES)
+        .resizable(false)
+        .window_size((400.0, 400.0))
+        .run()
 }
 
+/// State transitions
 #[derive(Debug, Clone)]
 enum Message {
-    TabSelected(usize),
-    TabClosed(usize),
-    TabLabelInputChanged(String),
-    TabContentInputChanged(String),
-    NewTab,
+    TabSelected(Tab),
 }
 
+/// Global app state
 #[derive(Debug, Default)]
-struct TabBarExample {
-    active_tab: usize,
-    new_tab_label: String,
-    new_tab_content: String,
-    tabs: Vec<(String, String)>,
+struct State {
+    active_tab: Tab,
 }
 
-impl TabBarExample {
+impl State {
     fn update(&mut self, message: Message) {
         match message {
-            Message::TabSelected(index) => {
-                println!("Tab selected: {}", index);
-                self.active_tab = index
-            }
-            Message::TabClosed(index) => {
-                self.tabs.remove(index);
-                println!("active tab before: {}", self.active_tab);
-                self.active_tab = if self.tabs.is_empty() {
-                    0
-                } else {
-                    usize::max(
-                        0,
-                        usize::min(self.active_tab, self.tabs.len() - 1),
-                    )
-                };
-                println!("active tab after: {}", self.active_tab);
-            }
-            Message::TabLabelInputChanged(value) => self.new_tab_label = value,
-            Message::TabContentInputChanged(value) => {
-                self.new_tab_content = value
-            }
-            Message::NewTab => {
-                println!("New");
-                if !self.new_tab_label.is_empty()
-                    && !self.new_tab_content.is_empty()
-                {
-                    println!("Create");
-                    self.tabs.push((
-                        self.new_tab_label.to_owned(),
-                        self.new_tab_content.to_owned(),
-                    ));
-                    self.new_tab_label.clear();
-                    self.new_tab_content.clear();
-                }
-            }
+            Message::TabSelected(index) => self.active_tab = index,
         }
     }
 
     fn view(&self) -> Element<Message> {
         Column::new()
             .push(
-                Row::new()
-                    .push(
-                        TextInput::new("Tab label", &self.new_tab_label)
-                            .on_input(Message::TabLabelInputChanged)
-                            .size(16)
-                            .padding(5.0),
-                    )
-                    .push(
-                        TextInput::new("Tab content", &self.new_tab_content)
-                            .on_input(Message::TabContentInputChanged)
-                            .size(12)
-                            .padding(5.0),
-                    )
-                    .push(
-                        Button::new(Text::new("New")).on_press(Message::NewTab),
-                    )
-                    .align_y(Alignment::Center)
-                    .padding(10.0)
-                    .spacing(5.0),
-            )
-            .push(
-                self.tabs
-                    .iter()
-                    .fold(
-                        TabBar::new(Message::TabSelected),
-                        |tab_bar, (tab_label, _)| {
-                            // manually create a new index for the new tab
-                            // starting from 0, when there is no tab created yet
-                            let idx = tab_bar.size();
-                            tab_bar
-                                .push(idx, TabLabel::Text(tab_label.to_owned()))
-                        },
-                    )
+                // Build the tab bar
+                Tab::iter()
+                    .fold(TabBar::new(Message::TabSelected), |tab_bar, tab| {
+                        tab_bar.push(tab, TabLabel::Text(tab.to_string()))
+                    })
                     .set_active_tab(&self.active_tab)
-                    .on_close(Message::TabClosed)
-                    .tab_width(Length::Shrink)
-                    .spacing(5.0)
+                    // Fill the entire screen evenly
+                    .tab_width(Length::FillPortion(Tab::iter().count() as u16))
                     .padding(5.0)
                     .text_size(32.0),
             )
-            .push(
-                if let Some((_, content)) = self.tabs.get(self.active_tab) {
-                    Text::new(content)
-                } else {
-                    Text::new("Please create a new tab")
-                }
-                .size(25),
-            )
             .into()
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
+enum Tab {
+    #[default]
+    Weather,
+    Transit,
+}
+
+impl Tab {
+    fn iter() -> impl Iterator<Item = Self> {
+        [Self::Weather, Self::Transit].into_iter()
+    }
+}
+
+impl Display for Tab {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Tab::Weather => write!(f, "Weather"),
+            Tab::Transit => write!(f, "Transit"),
+        }
     }
 }
